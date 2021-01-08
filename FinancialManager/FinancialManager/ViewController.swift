00bd4d79a43098eceffc3e57bd70bbc21d447d14
Anderson Alencar <andersonalencar@MacBook-Pro-de-Anderson.local>
2021-01-08 09:52:29 -0300

    add firebase and start project in xcode

---
+++ FinancialManager/FinancialManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinancialManager
+//
+//  Created by Anderson Alencar on 08/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
