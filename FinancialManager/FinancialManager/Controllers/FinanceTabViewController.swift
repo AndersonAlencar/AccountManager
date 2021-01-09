a62f158710d41c6db240b6b25e618117cb71342b
Anderson Alencar <andersonalencar@MacBook-Pro-de-Anderson.local>
2021-01-08 23:18:45 -0300

    set icons and color Tabbar

--- FinancialManager/FinancialManager/Controllers/FinanceTabViewController.swift
+++ FinancialManager/FinancialManager/Controllers/FinanceTabViewController.swift
@@ -12,18 +12,12 @@ class FinanceTabViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light
+        tabBar.barTintColor = .secondaryColor
+        tabBar.isTranslucent = false
+        
+        tabBar.tintColor = .orange
+        tabBar.unselectedItemTintColor = .lightGray
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
